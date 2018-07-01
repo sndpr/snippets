04a41b8ba05a8a4b6828ad469a3e98d5e1d2d292
sykq <genericmailacc@gmail.com>
2018-07-01 21:16:51 +0200

    wsclient

---
+++ src/main/java/org/psc/misc/ws/WsClient.java
@@ -0,0 +1,44 @@
+package org.psc.misc.ws;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WsClient {
+	private static final Logger LOGGER = LoggerFactory.getLogger(WsClient.class);
+
+	public static void main(String[] args) throws IOException {
+		WsClient.callWorker();
+	}
+
+	public static void callWorker() throws IOException {
+		URL url = new URL("http://localhost:8080/workerapp/worker");
+
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+		connection.setRequestMethod("GET");
+		connection.connect();
+		int responseCode = connection.getResponseCode();
+		String response = WsClient.read(connection.getInputStream());
+		connection.disconnect();
+		LOGGER.info("HTTP {} - {}", String.valueOf(responseCode), response);
+	}
+
+	private static String read(InputStream inputStream) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+		String line;
+		StringBuffer content = new StringBuffer();
+		while ((line = bufferedReader.readLine()) != null) {
+			content.append(line);
+		}
+		bufferedReader.close();
+		return content.toString();
+	}
+}
