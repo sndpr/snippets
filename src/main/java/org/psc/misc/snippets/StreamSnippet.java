cc0fd56132934d7dae27753ec4ff8ea2ab5e3add
sykq <genericmailacc@gmail.com>
2018-07-15 12:08:58 +0200

    misc

---
+++ src/main/java/org/psc/misc/snippets/StreamSnippet.java
@@ -0,0 +1,39 @@
+package org.psc.misc.snippets;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class StreamSnippet {
+	private static final Logger LOGGER = LoggerFactory.getLogger(StreamSnippet.class);
+
+	public static void main(String[] args) throws MalformedURLException {
+
+		List<String> stringList = List.of("abc", "def", "ghi", "jklmnopq");
+
+		AtomicInteger counter = new AtomicInteger(0);
+		List<ValueHolder> values = stringList.stream()
+				.map(e -> new ValueHolder(counter.getAndIncrement(), e)).collect(Collectors.toList());
+
+		values.forEach(e -> LOGGER.info("{} - {}", e.id, e.value));
+		
+		var someVar = new URL("http://www.youtube.com");
+		
+		LOGGER.info(someVar.toString());
+	}
+
+	@Data
+	@AllArgsConstructor
+	private static class ValueHolder {
+		int id;
+		String value;
+	}
+}
