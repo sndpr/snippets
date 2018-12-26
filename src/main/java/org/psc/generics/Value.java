b295ddb5e17beb2cbc8bb2da2a330a5f876523ef
sykq <genericmailacc@gmail.com>
2018-12-26 18:06:39 +0100

    misc

---
+++ src/main/java/org/psc/generics/Value.java
@@ -0,0 +1,26 @@
+package org.psc.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Value<T> {
+	private T value;
+	
+	public static void main(String[] args) {
+		List<Value<?>> list = new ArrayList<>();
+		list.add(new Value<String>("sadasd"));
+		list.add(new Value<Integer>(555));
+		
+		String s = (String) list.get(0).getValue();
+		System.out.println(s);
+		Integer i = (Integer) list.get(1).getValue();
+		System.out.println(i);
+	}
+}
