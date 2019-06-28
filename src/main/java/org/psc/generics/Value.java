b1c7bf72fc838341aa56080e8607cf0e20f26d4f
sykq <genericmailacc@gmail.com>
2019-06-28 19:10:02 +0200

    misc

--- src/main/java/org/psc/generics/Value.java
+++ src/main/java/org/psc/generics/Value.java
@@ -6,21 +6,22 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Value<T> {
-	private T value;
-	
-	public static void main(String[] args) {
-		List<Value<?>> list = new ArrayList<>();
-		list.add(new Value<String>("sadasd"));
-		list.add(new Value<Integer>(555));
-		
-		String s = (String) list.get(0).getValue();
-		System.out.println(s);
-		Integer i = (Integer) list.get(1).getValue();
-		System.out.println(i);
-	}
+    private T value;
+
+    public static void main(String[] args) {
+        List<Value<?>> list = new ArrayList<>();
+        list.add(new Value<String>("sadasd"));
+        list.add(new Value<Integer>(555));
+
+        String s = (String) list.get(0).getValue();
+        System.out.println(s);
+        Integer i = (Integer) list.get(1).getValue();
+        System.out.println(i);
+    }
 }
