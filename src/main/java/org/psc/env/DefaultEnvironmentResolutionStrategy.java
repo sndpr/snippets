1e0bfe6cd95c05a5ba0d83a4230d21e2bd007b7e
sykq <genericmailacc@gmail.com>
2019-10-13 20:01:26 +0200

    misc

--- src/main/java/org/psc/env/DefaultEnvironmentResolutionStrategy.java
+++ src/main/java/org/psc/env/DefaultEnvironmentResolutionStrategy.java
@@ -6,9 +6,14 @@ public class DefaultEnvironmentResolutionStrategy<T> implements EnvironmentAware
 
     public static final String ENVIRONMENT_PROPERTY_KEY = "environment";
 
+    private final String environment;
+
+    DefaultEnvironmentResolutionStrategy() {
+        environment = System.getProperty(ENVIRONMENT_PROPERTY_KEY).toLowerCase();
+    }
+
     @Override
     public T apply(Map<String, T> properties) {
-        String environment = System.getProperty(ENVIRONMENT_PROPERTY_KEY).toLowerCase();
         return properties.getOrDefault(environment, properties.get(""));
     }
 }
