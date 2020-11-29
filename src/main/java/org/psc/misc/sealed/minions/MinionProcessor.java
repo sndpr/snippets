a81f3b2a1e5868f44de60144eac9f206d9be85f1
sykq <genericmailacc@gmail.com>
2020-11-29 12:02:29 +0100

    add nonsensical sealed type structure as practice

--- src/main/java/org/psc/misc/sealed/minions/MinionProcessor.java
+++ src/main/java/org/psc/misc/sealed/minions/MinionProcessor.java
@@ -2,13 +2,22 @@ package org.psc.misc.sealed.minions;
 
 public class MinionProcessor {
 
-    public String applyEffect(Minion minion){
+    public String applyEffect(Minion minion) {
+
+        // this doesn't work with Java 15; maybe in a future release?
+        //        int attack = switch (minion) {
+        //            case Beast beast  -> beast.getAttack() * 2;
+        //            case Dragon dragon -> dragon.getAttack() * 3;
+        //            case SpecialMinion specialMinion -> attack = Math.max(specialMinion.getAttack() - 1, 0);
+        //            default -> minion.getAttack();
+        //        };
+
         int attack;
-        if (minion instanceof Beast beast){
+        if (minion instanceof Beast beast) {
             attack = beast.getAttack() * 2;
-        } else if (minion instanceof Dragon dragon){
+        } else if (minion instanceof Dragon dragon) {
             attack = dragon.getAttack() * 3;
-        } else if(minion instanceof SpecialMinion specialMinion){
+        } else if (minion instanceof SpecialMinion specialMinion) {
             attack = Math.max(specialMinion.getAttack() - 1, 0);
         } else {
             throw new IllegalArgumentException("given minion type not supported");
