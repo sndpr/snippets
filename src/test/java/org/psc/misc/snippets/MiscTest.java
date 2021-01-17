794c0e8d68006e6db28826d360abc2675b572e8b
sykq <genericmailacc@gmail.com>
2021-01-17 11:11:47 +0100

    process program arguments of k-invoker

--- src/test/java/org/psc/misc/snippets/MiscTest.java
+++ src/test/java/org/psc/misc/snippets/MiscTest.java
@@ -4,7 +4,6 @@ import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MiscTest {
 
