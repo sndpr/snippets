7dbf79b238cf808d2cf8be4e7d6f9a1ededb38ee
sykq <genericmailacc@gmail.com>
2021-01-17 11:40:06 +0100

    update Lombok @With

--- src/main/java/org/psc/misc/snippets/DefaultPojo.java
+++ src/main/java/org/psc/misc/snippets/DefaultPojo.java
@@ -1,15 +1,15 @@
 package org.psc.misc.snippets;
 
-import java.math.BigInteger;
-import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.Wither;
+import lombok.With;
+
+import java.math.BigInteger;
+import java.util.List;
 
 @Data
-@Wither
+@With
 @AllArgsConstructor
 @NoArgsConstructor
 public class DefaultPojo {
