7694d751acf6771cb412e54f299bc9e92f78f015
sndpg <genericmailacc@gmail.com>
2022-10-02 12:27:57 +0200

    cleanup

--- src/main/java/org/psc/misc/snippets/NumberCategorizer.java
+++ src/main/java/org/psc/misc/snippets/NumberCategorizer.java
@@ -1,8 +1,6 @@
 package org.psc.misc.snippets;
 
-import java.awt.datatransfer.StringSelection;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class NumberCategorizer {
