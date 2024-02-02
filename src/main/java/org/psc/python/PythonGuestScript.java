95c4105148558f1c4af06f76b914185d3c959288
sndpg <genericmailacc@gmail.com>
2024-02-02 20:47:39 +0100

    graalvm guest scripts

--- src/main/java/org/psc/python/PythonGuestScript.java
+++ src/main/java/org/psc/python/PythonGuestScript.java
@@ -2,11 +2,12 @@ package org.psc.python;
 
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Value;
+import org.graalvm.polyglot.proxy.ProxyArray;
 
 public class PythonGuestScript {
 
     public static void main(String[] args) {
-        try (Context context = Context.create()) {
+        try (Context context = Context.create("python")) {
             Value result = context.eval(
                 "python",
                 """
@@ -38,12 +39,28 @@ public class PythonGuestScript {
             long aLong = someAddition.invokeMember("some_fun").asLong();
             System.out.println(aLong);
 
-            Value additionWithParams = context.eval("python", """
-                def some_fun(a, b):
+            Value evaluatedContext = context.eval("python", """
+                def add(a, b):
                     return a + b
+                    
+                # does it know accept annotations?
+                def greet(name: str) -> str:
+                    print('hi from python')
+                    return f'Hello {name}!'
+                    
+                    
+                def sum_of_squares(ints: [int]) -> int:
+                    return sum( i * i for i in ints)
                 """);
-            long anotherLong = additionWithParams.invokeMember("some_fun", 51561, 8978).asLong();
+            long anotherLong = evaluatedContext.invokeMember("add", 51561, 8978).asLong();
             System.out.println(anotherLong);
+
+            var greeting = evaluatedContext.invokeMember("greet", "world").asString();
+            System.out.println(greeting);
+
+            var sumOfSquares =
+                evaluatedContext.invokeMember("sum_of_squares", ProxyArray.fromArray(2, 2, 3, 2, 5)).asLong();
+            System.out.println(sumOfSquares);
         }
     }
 }
